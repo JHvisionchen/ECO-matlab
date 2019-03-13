56d62e7fab8a7d3b5520cacaf7b5692eb1b971d2
JHvisionchen <chenjiahong@163.com>
2019-03-13 10:45:59 +0800

    first commit

---
+++ demo_ECO_gpu.m
@@ -0,0 +1,15 @@
+
+% This demo script runs the ECO tracker with deep features on the
+% included "Crossing" video.
+
+% Add paths
+setup_paths();
+
+% Load video information
+base_path = '/media/cjh/datasets/tracking/OTB100/';
+video = choose_video(base_path);
+video_path=[base_path video];
+[seq, ground_truth] = load_video_info(video_path);
+
+% Run ECO
+results = testing_ECO_gpu(seq);
